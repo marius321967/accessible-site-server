162f6531d75f9e94e7b66ccd5770ba49e53065f4
Marius Drąsutis <marius.dras@gmail.com>
2019-12-06 14:08:56 +0200

    Added user-required middleware

---
+++ middleware/protected_user.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const secret = require('../config.json').jwt_secret;
+const get_user_by_id = require('../database/users/get_by_id');
+
+module.exports = (req, res, next) => {
+    const line = req.header('Authorization');
+
+    if (!line) return res.status(401).send({ err: 'AUTH_HEADER_MISSING' });
+
+    const match = line.match(/^Bearer (.+)$/);
+    if (match === null) return res.status(401).send({ err: 'INCORRECT_HEADER_FORMAT_NON_BEARER' });
+
+    const token = match[1];
+    try { var body = jwt.verify(token, secret) }
+    catch (e) { return res.status(401).send({ err: 'TOKEN_INVALID' }) }
+
+    get_user_by_id(body.sub)
+    .then(user => {
+        if (user === null) return res.status(401).send({ err: 'USER_NOT_FOUND' });
+        
+        req.user = user;
+        next();
+    })
+}
