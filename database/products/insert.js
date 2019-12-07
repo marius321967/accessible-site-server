d0e39912b32091ee25c025cdc6d18413acee9b3c
Marius Drąsutis <marius.dras@gmail.com>
2019-12-07 21:19:02 +0200

    Remove log

--- database/products/insert.js
+++ database/products/insert.js
@@ -5,8 +5,6 @@ const repository = require('../');
  */
 module.exports = product => {
     return new Promise((resolve, reject) => {
-        console.log('inesrting', product);
-        
         repository.database.collection('products').insertOne(product, (err, result) => {
             if (err) return reject(err);
             
