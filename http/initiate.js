000bafafe6c36c62bc03b6c315d8b27c59e689ea
Marius Drąsutis <marius.dras@gmail.com>
2019-12-08 22:14:41 +0200

    app.use for file upload reading

--- http/initiate.js
+++ http/initiate.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('../routes');
+const fileUpload = require('express-fileupload');
 
 /**
  * Helper for initiating the HTTP service.
@@ -11,6 +12,7 @@ module.exports = () => {
 
     app.use(express.json());
     app.use(cors());
+    app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
 
     routes(app);
 
