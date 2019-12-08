cb3f6b495e6c4de51df0515215d5be796b316652
Marius Drąsutis <marius.dras@gmail.com>
2019-12-08 17:57:57 +0200

    Fixed all product retrieval route, removed console log call

--- routes/admin/products/all.js
+++ routes/admin/products/all.js
@@ -1,3 +1,3 @@
 get_all_products = require('../../../database/products/get_all');
 
-module.exports = (req, res) => get_all_products.then(products => res.send(products));
+module.exports = (req, res) => get_all_products().then(products => res.send(products));
